28e5b34cee8b315fa6ec18e866a9ff7fd9986457
Kuldeep Singh Rai <imkuldeepsinghrai@gmail.com>
2021-11-23 12:26:36 +0530

    Added SearchView to search for specific News

--- app/src/main/java/com/ksr/newsapp_restapi_project/Activities/MainActivity.java
+++ app/src/main/java/com/ksr/newsapp_restapi_project/Activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ksr.newsapp_restapi_project.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.ksr.newsapp_restapi_project.API.ApiRequestManager;
 import com.ksr.newsapp_restapi_project.API.OnFetchDataListener;
@@ -25,12 +27,14 @@ public class MainActivity extends AppCompatActivity implements SelectListener , 
     NewsAdapter newsAdapter;
     ProgressDialog dialog;
     Button btnBusiness, btnEntertainment,btnGeneral,btnHealth,btnScience,btnSports,btnTechnology;
-
+    String newsCategory = "general";
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         dialog = new ProgressDialog(MainActivity.this);
         dialog.setTitle("Fetching News Articles...");
@@ -53,23 +57,42 @@ public class MainActivity extends AppCompatActivity implements SelectListener , 
         btnSports.setOnClickListener(this);
         btnTechnology.setOnClickListener(this);
 
+        searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                ApiRequestManager apiRequestManager = new ApiRequestManager(MainActivity.this);
+                apiRequestManager.getNewsArticles(listener, newsCategory, query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
 
         ApiRequestManager apiRequestManager = new ApiRequestManager(this);
-        apiRequestManager.getNewsArticles(listener, "general", null);
+        apiRequestManager.getNewsArticles(listener, newsCategory, null);
 
     }
 
     private final OnFetchDataListener<NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>() {
         @Override
         public void onFetchData(List<Articles> list, String message) {
-            showNews(list);
-            dialog.dismiss();
+            if (list.isEmpty()){
+                Toast.makeText(MainActivity.this, "No Data Found!!", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                showNews(list);
+                dialog.dismiss();
+            }
         }
 
         @Override
         public void onError(String message) {
-
+            Toast.makeText(MainActivity.this, "An Error Occured!!", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -96,6 +119,7 @@ public class MainActivity extends AppCompatActivity implements SelectListener , 
     public void onClick(View view) {
         Button button = (Button) view;
         String category = button.getText().toString();
+        newsCategory = category;
         dialog.setTitle("Fetching News Articles of "+category+" ...");
         dialog.show();
         ApiRequestManager apiRequestManager = new ApiRequestManager(this);
